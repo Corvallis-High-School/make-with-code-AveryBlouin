d4280e31ae834aaa3bedb33940ed31e6b1f1aa2a
Avery Blouin <avery.blouin@student.csd509j.net>
2025-06-10 15:09:18 -0700

    make repl

--- consolesim.lua
+++ consolesim.lua
@@ -6,6 +6,11 @@ end
 local console = {}
 console.__index = console
 
+function console:render()
+    cls()
+    io.write(self.text)
+end
+
 function console:readchar()
     local char = table.remove(self.buffer,1)
     if char then return char end
@@ -14,13 +19,11 @@ function console:readchar()
     if read == "" or read == nil then read = "\0" end
 
     self.text = self.text.. read
-
     for c in string.gmatch(read,".") do
         table.insert(self.buffer,c)
     end
 
-    cls()
-    io.write(self.text)
+    self:render()
 
     return table.remove(self.buffer,1)
 end
@@ -30,21 +33,29 @@ function console:display(txt)
     io.write(txt)
 end
 
-function console.new(input)
-    input = input or ""
-    local self = {}
+function console:addText(txt)
+    self.text = self.text.. txt
+end
 
-    self.text = "Console mode simulator\nReturn must be pressed after typing a character (can't do anything about that),\nleaving empty will result in a null character.\nInputting extra characters will add them to a buffer.\n\n".. input
-    self.buffer = {}
-
+function console:addUserInput(input)
     for c in string.gmatch(input,".") do
         table.insert(self.buffer,c)
     end
+end
 
-    cls()
-    io.write(self.text)
+function console.new(input)
+    input = input or ""
+    local self = setmetatable({},console)
 
-    return setmetatable(self,console)
+    self.text = "Console mode simulator\nReturn must be pressed after typing a character (can't do anything about that),\nleaving empty will result in a null character.\nInputting extra characters will add them to a buffer.\n\n"
+    self.buffer = {}
+
+    self.text = self.text.. input
+    self:addUserInput(input)
+
+    self:render()
+
+    return self
 end
 
 return console
